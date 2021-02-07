3b40f864d2f04a81b80c96026bebe9ec4e8cb9f5
NguyenTra1103 <thutrathao@gmail.com>
2021-02-07 15:15:51 +0700

    no message

--- training/codelabs_k2_day4/codelabs_day4_Tra/main.js
+++ training/codelabs_k2_day4/codelabs_day4_Tra/main.js
@@ -1,39 +1,44 @@
-function myfunction() {
+function Comment_Item() {
     var main5Element = document.querySelector('.main5 ');
     Object.assign(main5Element.style, { height: '650px', });
     var commentElement = document.querySelector('.list_comment ');
     Object.assign(commentElement.style, {
-        display: 'block',
+        display: 'block'
     });
 }
 
-function myfunction1() {
-    var main1Element = document.querySelector('.click_hidden');
-    Object.assign(main1Element.style, { display: 'block', });
-    var main11Element = document.querySelector('.click_main1');
-    Object.assign(main11Element.style, { display: 'none', });
+function show_more_less() {
+    var showmore = document.querySelector('.click_hidden');
+    var item = document.querySelector('.fa-angle-down')
+        /*Object.assign(showmore.style, { display: 'block', });*/
+    var text = document.getElementById('.main_text');
+    if (item.style.display === 'none') {
+        item.style.display = 'inline',
+            main_text.innerHTML = 'Xem thêm',
+            showmore.style.display = 'none'
+
+    } else {
+        item.style.display = 'none',
+            main_text.innerHTML = 'Ẩn bớt',
+            showmore.style.display = 'inline'
+    }
+
     var main12Element = document.querySelector('.main1');
     Object.assign(main12Element.style, {
-        height: '1200px',
-        /*overflow: 'scroll',*/
-        position: 'relative'
+        overflow: 'auto',
+        width: '33.33333 %',
 
     });
-    var main123Element = document.querySelector('.main2');
-    Object.assign(main123Element.style, { position: 'fixed' });
+    /* var main123Element = document.querySelector('.main2');
+    Object.assign(main123Element.style, { position: 'fixed' });*/
 }
 
 function myfunction2() {
     var main123Element = document.querySelector('.listmain3_click');
     Object.assign(main123Element.style, { display: 'block' });
-    var main124Element = document.querySelector('.main1');
-    Object.assign(main124Element.style, {
-        height: '1200px'
-            /*,
-                    overflow: 'scroll'*/
-    });
-    var main1234Element = document.querySelector('.main2');
-    Object.assign(main1234Element.style, { position: 'fixed' });
+
+    /* var main1234Element = document.querySelector('.main2');
+    Object.assign(main1234Element.style, { position: 'fixed' });*/
     var clickElement = document.querySelector('.click_main2');
     Object.assign(clickElement.style, { display: 'none', });
 }
